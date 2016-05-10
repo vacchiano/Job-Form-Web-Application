e0c2ec3c5bb1fa122998fc67ee3a6d85f15455ca
Dom Vacchiano <djvacchiano@gmail.com>
2016-05-10 16:54:48 -0500

    Add files via upload

---
+++ lab7.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Thank You For Submitting Your Zoo Application! </br>
+Name: <?php echo $_POST["name"]; ?><br>
+Telephone: <?php echo $_POST["telephone"]; ?><br>
+e-mail:  <?php echo $_POST["email"]; ?>br>
+Birthdate: <?php echo $_POST["birthDate"]; ?><br>
+Gender: <?php echo $_POST["gender"]; ?><br>
+Comments: <?php echo $_POST["comments"]; ?>
+
+</body>
+</html>
